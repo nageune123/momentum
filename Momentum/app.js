b725e1490e88ab646f6bdb1d4df826310277000c
nageune123 <120440555+nageune123@users.noreply.github.com>
2022-12-21 18:54:10 +0900

    Add files via upload

---
+++ Momentum/app.js
@@ -0,0 +1,17 @@
+const player={
+    name:"nico",
+    points:10,
+    fat: true
+
+}
+
+console.log(player);
+console.log(player.name);
+player.fat=false;  
+player.lastname="potato"
+player.points=player.points+15;
+
+console.log(player);
+
+
+
